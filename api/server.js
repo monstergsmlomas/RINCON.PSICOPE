e060bd83e0bb9ef407ebd2b9f60f5353b3161026
monstergsmlomas <monstergsmlomas@gmail.com>
2026-03-15 00:51:20 -0300

    add products endpoint

--- api/server.js
+++ api/server.js
@@ -6,9 +6,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// ===== PRODUCTOS =====
+
+const products = [
+{
+id:1,
+title:"Guía práctica de Evaluación Psicopedagógica",
+price:19000
+},
+{
+id:2,
+title:"Kit de Informes Psicopedagógicos",
+price:24000
+},
+{
+id:3,
+title:"Cuadernillo de Técnicas de Intervención",
+price:22000
+}
+];
+
+
+// ===== RUTA PRINCIPAL =====
+
 app.get("/", (req, res) => {
   res.send("API Rincón Psicope funcionando");
 });
+
+
+// ===== OBTENER PRODUCTOS =====
+
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+
+
+// ===== CHECKOUT =====
 
 app.post("/checkout", (req, res) => {
 
@@ -22,6 +56,9 @@ app.post("/checkout", (req, res) => {
   });
 
 });
+
+
+// ===== SERVIDOR =====
 
 const PORT = process.env.PORT || 10000;
 
